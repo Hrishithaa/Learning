0a1240dd497236e4e1225f3b95d532ae37d4ebca
Hrishithaa <ahrishitha13@gmail.com>
2025-03-04 15:14:07 +0530

    4/3/25

---
+++ practise.js
@@ -0,0 +1,4 @@
+var aEmp=[{"EmpId": 1, "Name": "hrishi", "Salary" : 1000, "Currency" : "AED"},
+          {"EmpId": 2, "Name": "rounith", "Salary" : 2000, "Currency" : "USD"},
+          {"EmpId": 3, "Name": "shreya", "Salary" : 3000, "Currency" : "INR"}
+        ];
